e044d1ad32c97e3682121a2875543df5e33e3082
pi <644607120003@dpu.ac.th>
2023-07-18 18:07:41 +0700

    robotPi

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import math
 import threading
 import json
+import multiprocessing
 
 import gps
 import WaypointNavigationAlgorithm as wna
@@ -12,70 +13,64 @@ import move_robot as mr
 
 class main:
 	
-	def __init__(self,hostname,username,password,topic):
+	def __init__(self,hostname,username,password,topic,posrobot):
 		self.hostname = hostname
 		self.username = username
 		self.password = password
 		self.topic = topic
 		self.file_kml = "waypoint.kml"
 		self.pos = {}
+		self.posrobot = posrobot
 		self.travel = {}
-		self.waypoint1 = gps.GPSModule().read_gps_data()
 		self.BMM150 = 90
+		#self.posrobot = [0,0]
 			
 	def pud_thread(self):
-		for i in self.gps_threading():
-			i = json.JSONEncoder().encode(i)
+		i = json.JSONEncoder().encode(self.pos)
 			#j = json.JSONEncoder().encode(j)
-			mqtt.MQTT_R(self.hostname,self.username,self.password).publish_single(self.topic[0],i)
+		mqtt.MQTT_R(self.hostname,self.username,self.password).publish_single(self.topic[0],i)
 			#mqtt.MQTT_R(self.hostname,self.username,self.password).publish_single(topic[1],j)
-			print(i)
+			#print(i)
 			
-	def robot_move(self):
-        all_path =self.cal_thread().copy()
-        robot = mr.Robot(0.7, 0.084, self.waypoint1, all_path[0], all_path[1], self.BMM150)
-        while True:
-            if len(all_path) > 0:
-                robot.follow_path()
-                robot.move()
-                
-                if wna.Waypoint(self.waypoint1, all_path[0][0]) < 0.0001:
-                    all_path[0].pop(0)
-                    all_path[1].pop(0)
+	def robot_move(self, L, r,path):
+		# self.waypoint1 = gps.GPSModule().read_gps_data()
+		robot = mr.Robot(L, r, self.posrobot, path[0], path[1], self.BMM150)
+		if len(all_path) > 0:
+			robot.follow_path()
+			robot.move()
+			print(robot.move(), robot.follow_path())
+			if wna.Waypoint(self.pos, all_path[0][0]).haversine() < 0.0001:
+				path[0].pop(0)
+				path[1].pop(0)
+		print(self.pos)
 	
 	def cal_thread(self):
-		self.waypoint1
-		while self.waypoint1[0] == 0:
-			self.waypoint1 = gps.GPSModule().read_gps_data()
+		if self.posrobot[0] != 0 and self.posrobot[1] != 0:
+			waypoints = self.kml_thread().copy()
+			waypoints.insert(0,self.posrobot)
+			Nwaypoints = []
+			theta = []
+			for i in range(len(waypoints)-1):
+				start = waypoints[i]
+				goal = waypoints[i+1]
+				set_upcal = wna.Waypoint(start,goal)
+				set_upcal.haversine()
+				set_upcal.bearing()
+				des = set_upcal.destination(start)
+				#print("All polt:", (haver, bear, des))
+				Nwaypoints.append(start)
+				theta.append(int(set_upcal.bearing()[1]))
+				
+				for j in range(des[0]):
+					#print("Process point:", j+1)
+					point = des[2::]
+					Nwaypoints.append(point)
+					#print(point)
+					des = set_upcal.destination(point)
 			
-		waypoints = self.kml_thread().copy()
-		waypoints.insert(0,self.waypoint1)
-		Nwaypoints = []
-		theta = []
-		for i in range(len(waypoints)-1):
-			start = waypoints[i]
-			goal = waypoints[i+1]
-			set_upcal = wna.Waypoint(start,goal)
-			set_upcal.haversine()
-			set_upcal.bearing()
-			des = set_upcal.destination(start)
-			#print("All polt:", (haver, bear, des))
-			Nwaypoints.append(start)
-			theta.append(int(set_upcal.bearing()))
-			
-			for j in range(des[0]):
-				#print("Process point:", j+1)
-				point = des[2::]
-				Nwaypoints.append(point)
-				#print(point)
-				des = set_upcal.destination(point)
-		
-		Nwaypoints.append(len(waypoints)-1)
-		print(Nwaypoints)
-			
-		
-		
-		return (Nwaypoints, theta)
+			Nwaypoints.append(waypoints[len(waypoints)-1])
+			#print(Nwaypoints)
+			return (Nwaypoints, theta)
 	
 	def kml_thread(self):
 		waypoints = []
@@ -84,35 +79,57 @@ class main:
 		return waypoints
 	
 	def gps_threading(self):
-		while True:
-			self.waypoint1 = gps.GPSModule().read_gps_data()
 				
-			if (self.waypoint1[0] != 0) and (self.waypoint1[1] != 0):
-				self.pos["name"] = "robotgps"
-				self.pos["lat"] = self.waypoint1[0]
-				self.pos["lon"] = self.waypoint1[1]
-				#selftravel["dis"] = dis
-				#self.travel["ber"] = round(ber)
-				yield self.pos
+		if (self.posrobot[0] != 0) and (self.posrobot[1] != 0):
+			self.pos["name"] = "robotgps"
+			self.pos["lat"] = self.posrobot[0]
+			self.pos["lon"] = self.posrobot[1]
+			#selftravel["dis"] = dis
+			#self.travel["ber"] = round(ber)
+			return self.pos
 	
 	
 
-if __name__ == "__main__"	:	
+if __name__ == "__main__"	:
 	hostname = "localhost"
 	username = "robotgps"
 	password = "RIPzero27413."	
 	#kml_file = "waypoint.kml"
-	main_f = main(hostname,username,password,["Robot/GPS","Robt/Travel"])
-	print(main_f.cal_thread())
+	posGPS = gps.GPSModule()
+	running = True
+	#main_f.cal_thread()
+	waypointpath = []
+	#p1 = multiprocessing.Process(target=main_f.robot_move(), args=(0.7, 0.84,waypointpath))
+	#p1.start()
+	#p1.join()
+	while running:
+		posRobot = posGPS.read_gps_data()
+		main_f = main(hostname,username,password,["Robot/GPS","Robt/Travel"], posRobot)
+		main_f.gps_threading()
+		main_f.pud_thread()
+		#print(main_f.pos)
+		if waypointpath is None:
+			waypointpath = main_f.cal_thread()
+		
+		#if len(waypointpath) == 0:
+		#	waypointpath = main_f.cal_thread()
+		
+		if len(waypointpath) > 0 :
+			main_f.robot_move(0.5, 0.08,waypointpath)
+			
+		print(len(waypointpath))
+			
+	
+	#print(main_f.cal_thread()[1])
 	#t1 = threading.Thread(target=gps_threading, args=([13.720302, 100.570397],))
-	t1 = threading.Thread(target=main_f.pud_thread)
+	#t1 = threading.Thread(target=main_f.pud_thread)
 	#t1 = gps_main.pud_thread(hostname,username,password,["Robot/GPS","Robt/Travel"])
-	#t2 = threading.Thread(target=main_f.cal_thread)
+	#t2 = threading.Thread(target=main_f.robot_move)
 
-	t1.start()
+	#t1.start()
 	#t2.start()
 
-	t1.join()
+	#t1.join()
 	#t2.join()
 	#for i in gps_threading([13.720302, 100.570397]):
 	#	print(i)
