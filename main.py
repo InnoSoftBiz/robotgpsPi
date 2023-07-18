1d5a75b42b8d7a4a67a2d510fbfe5113225fbd51
pi <644607120003@dpu.ac.th>
2023-07-18 14:12:08 +0700

    robotPi

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import time
-import gps
 import math
+import threading
+import json
+
+import gps
 import WaypointNavigationAlgorithm as wna
 import mqtt
-import threading
 from multiprocessing import Process
-import json
 import File_kml as kml
+import move_robot as mr
 
 class main:
 	
@@ -19,19 +21,7 @@ class main:
 		self.pos = {}
 		self.travel = {}
 		self.waypoint1 = gps.GPSModule().read_gps_data()
-		
-		
-	def gps_threading(self):
-		while True:
-			self.waypoint1 = gps.GPSModule().read_gps_data()
-				
-			if (self.waypoint1[0] != 0) and (self.waypoint1[1] != 0):
-				self.pos["name"] = "robotgps"
-				self.pos["lat"] = self.waypoint1[0]
-				self.pos["lon"] = self.waypoint1[1]
-				#selftravel["dis"] = dis
-				#self.travel["ber"] = round(ber)
-				yield self.pos
+		self.BMM150 = 90
 			
 	def pud_thread(self):
 		for i in self.gps_threading():
@@ -40,12 +30,18 @@ class main:
 			mqtt.MQTT_R(self.hostname,self.username,self.password).publish_single(self.topic[0],i)
 			#mqtt.MQTT_R(self.hostname,self.username,self.password).publish_single(topic[1],j)
 			print(i)
-
-	def kml_thread(self):
-		waypoints = []
-		for i in kml.kml_to_csv(self.file_kml):
-			waypoints.append(i[1:])
-		return waypoints
+			
+	def robot_move(self):
+        all_path =self.cal_thread().copy()
+        robot = mr.Robot(0.7, 0.084, self.waypoint1, all_path[0], all_path[1], self.BMM150)
+        while True:
+            if len(all_path) > 0:
+                robot.follow_path()
+                robot.move()
+                
+                if wna.Waypoint(self.waypoint1, all_path[0][0]) < 0.0001:
+                    all_path[0].pop(0)
+                    all_path[1].pop(0)
 	
 	def cal_thread(self):
 		self.waypoint1
@@ -55,15 +51,17 @@ class main:
 		waypoints = self.kml_thread().copy()
 		waypoints.insert(0,self.waypoint1)
 		Nwaypoints = []
+		theta = []
 		for i in range(len(waypoints)-1):
 			start = waypoints[i]
 			goal = waypoints[i+1]
 			set_upcal = wna.Waypoint(start,goal)
-			haver = set_upcal.haversine()
-			bear = set_upcal.bearing()
+			set_upcal.haversine()
+			set_upcal.bearing()
 			des = set_upcal.destination(start)
 			#print("All polt:", (haver, bear, des))
 			Nwaypoints.append(start)
+			theta.append(int(set_upcal.bearing()))
 			
 			for j in range(des[0]):
 				#print("Process point:", j+1)
@@ -77,7 +75,26 @@ class main:
 			
 		
 		
-		return Nwaypoints
+		return (Nwaypoints, theta)
+	
+	def kml_thread(self):
+		waypoints = []
+		for i in kml.kml_to_csv(self.file_kml):
+			waypoints.append(i)
+		return waypoints
+	
+	def gps_threading(self):
+		while True:
+			self.waypoint1 = gps.GPSModule().read_gps_data()
+				
+			if (self.waypoint1[0] != 0) and (self.waypoint1[1] != 0):
+				self.pos["name"] = "robotgps"
+				self.pos["lat"] = self.waypoint1[0]
+				self.pos["lon"] = self.waypoint1[1]
+				#selftravel["dis"] = dis
+				#self.travel["ber"] = round(ber)
+				yield self.pos
+	
 	
 
 if __name__ == "__main__"	:	
---
+++ move_robot.py
@@ -0,0 +1,30 @@
+import  math
+
+class Robot:
+    
+    def __init__(self, L, r, pos_robot, path, theta_desired, theta_actual):
+        self.L = L
+        self.r = r
+        self.pos = pos_robot
+        self.path = path[0]
+        self.theta_d = theta_desired[0]
+        self.theta_a = theta_actual
+        self.theta_e = 0
+        self.vr = 0
+        self.vl = 0
+        self.u = 0
+        self.w = 0
+    
+    def move(self):
+        self.vr = self.u + ((self.L / 2) * self.w)
+        self.vl = self.u - ((self.L / 2) * self.w)
+        
+        return self.vr, self.vl
+    
+    def follow_path(self):
+        self.theta_e = math.atan2(math.sin(self.theta_d - self.theta_a),math.cos(self.theta_d - self.theta_a))
+        target_x, target_y = self.path
+        delta_x = target_x - self.pos[0]
+        delta_y = target_y - self.pos[1]
+        self.u = delta_x * math.cos(self.theta_e) + delta_y * math.sin(self.theta_e)
+        self.w = (-1 / self.r) * math.sin(self.theta_e) * delta_x + (1 / self.r) * math.cos(self.theta_e) * delta_y
