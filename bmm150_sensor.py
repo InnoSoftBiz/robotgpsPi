4775e2d81d9cccba6ccd7cb5e60dbc7ee4447b13
pi <644607120003@dpu.ac.th>
2023-07-20 17:44:40 +0700

    robotgpsPi

--- bmm150_sensor.py
+++ bmm150_sensor.py
@@ -3,6 +3,15 @@ import math
 
 device = bmm150.BMM150()
 
-x, y, z = device.read_mag_data()
+while True:
 
+	x, y, z = device.read_mag_data()
 
+	heading_rads = math.atan2(x, y)
+	heading_degrees = math.degrees(heading_rads)
+
+	print(f"X : {x:.2f}uT")
+	print(f"Y : {y:.2f}uT")
+	print(f"Z : {z:.2f}uT")
+
+	print(f"Heading : {(heading_degrees+ 360) % 360:.2f}Deg")
